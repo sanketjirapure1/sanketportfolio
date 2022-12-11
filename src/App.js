c8914711a5a62cd700375990936b53c0e37d5417
sanketjirapure10 <sanketjirapure10@gmail.com>
2022-12-11 10:53:43 +0530

    first commit in live website

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="color">sanket jirapure portfolio</h1>
+      <h1 className="color">sanket jirapure portfolio fist live websiten</h1>
     </div>                                                                                     
   );
 }
